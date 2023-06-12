3b67ec55d5ea649761158ded72c6b5684a899a1a
matheusjean <matheusjean11@hotmail.com>
2023-06-12 13:28:00 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecase/authentication/remote-authentication.spec.ts
+++ src/data/usecase/authentication/remote-authentication.spec.ts
@@ -1,18 +1,20 @@
 import { mockAuthentication } from '@/domain/test/mock-authentication';
 import { HttpPostClientSpy } from '../../test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
-import faker from 'faker'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { UnexpectedError } from '@/domain/errors/unexpected-error';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/models/account-model';
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
   return {
@@ -21,6 +23,7 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
   }
 }
 
+describe('Remote Authentication', () => {
   test('Should call HttpClient with correct URL', async () => {
     const url = faker.internet.url()
     const {sut, httpPostClientSpy} = makeSut(url)
@@ -70,3 +73,4 @@ const makeSut = (url: string = faker.internet.url()): SutTypes => {
     const promise = sut.auth(mockAuthentication())
     expect(promise).rejects.toThrow(new UnexpectedError())
   });
+});
