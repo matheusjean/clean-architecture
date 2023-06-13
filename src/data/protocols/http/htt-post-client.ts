c956326bb0d5507d761ce9e84b3d3dc114c72813
matheusjean <matheusjean11@hotmail.com>
2023-06-13 12:11:28 -0300

    feat: ensure AxiosHttpClient calls axios with correct verb

--- src/data/protocols/http/htt-post-client.ts
+++ src/data/protocols/http/htt-post-client.ts
@@ -2,10 +2,9 @@ import { HttpResponse } from '.'
 
 export type HttpPostParams<T> = {
   url: string
-  body: T
+  body?: T
 }
 
 export interface HttpPostClient<T, R> {
-  post (params: HttpPostParams<T>): Promise<HttpResponse<R>>
+  post(params: HttpPostParams<T>): Promise<HttpResponse<R>>
 }
-  
